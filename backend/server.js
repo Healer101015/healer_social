66edf8f7d3edaaec01b938b51196909a6f167749
Willian Massao Barbosa Sado <91169069+Willian-Massao@users.noreply.github.com>
2025-08-29 03:40:40 -0300

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -69,7 +69,10 @@ const upload = multer({
 });
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
 app.use(morgan("dev"));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
