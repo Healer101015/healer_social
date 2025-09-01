4f2fe6bae1515d08886256ff02005791a0b47f62
Willian Massao <brcraftgames@gmail.com>
2025-09-01 18:39:02 -0300

    another img host

--- image_uploader/server.js
+++ image_uploader/server.js
@@ -22,13 +22,34 @@ app.post('/upload', async (req, res) => {
     if (!imageBase64) {
         return res.status(400).json({ error: 'Missing imageBase64' });
     }
+
     try {
+        if(contentType.includes("image")){
+            const formatData = new formatData();
+
+            formatData.append('source', imageBase64);
+            const free_respose = await fetch("https://freeimage.host/api/1/upload/?key=6d207e02198a847aa98d0a2a901485a5", {
+                method: "POST",
+                body: formatData
+            })
+
+            const free_data = await free_respose.json();
+            if(free_data.status_code !== 200){
+                return res.status(500).json({ error: 'Failed to upload image to freeimage.host' });
+            }
+
+            return res.status(200).json({ message: 'Image uploaded successfully', url: free_data.image.display_url });
+        }
+
         const channel = await client.channels.fetch(process.env.DISCORD_CHANNEL_ID);
+
         if (!channel) {
             return res.status(404).json({ error: 'Channel not found' });
         }
+
         const buffer = Buffer.from(imageBase64, 'base64');
         const image_respose = await channel.send({ files: [{ attachment: buffer, name }] });
+        
         res.status(200).json({ message: 'Image uploaded successfully', url: image_respose.attachments.first().url });
     } catch (error) {
         console.error('Error uploading image:', error);
